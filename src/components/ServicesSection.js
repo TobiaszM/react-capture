72fb8ffcd084f5644961004010f4bc2a28e33b1a
tobiasz.mateja <tobiasz00@protonmail.com>
2021-02-09 21:47:34 +0100

    services section

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import clock from "../img/clock.svg";
+import diaphragm from "../img/diaphragm.svg";
+import money from "../img/money.svg";
+import teamwork from "../img/teamwork.svg";
+import home2 from "../img/home2.png";
+
+const ServiceSection = () => {
+    return(
+        <div className="sevices">
+            <div className="description">
+                <h2>High <span>quality</span> services</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+                            <img src={clock} alt="icon"/>
+                            <h3>Efficemt</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={teamwork} alt="icon"/>
+                            <h3>Teamwork</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={diaphragm} alt="icon"/>
+                            <h3>Diaphragm</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={money} alt="icon"/>
+                            <h3>Affordable</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+            <img src={home2}/>
+        </div>
+    )
+}
+export default ServiceSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServiceSection from "../components/ServicesSection";
 const AboutUs = () => {
-    return <AboutSection/>;
+    return(
+    <>
+        <AboutSection/>
+        <ServiceSection/>
+    </>
+    );
 }
 export default AboutUs;
