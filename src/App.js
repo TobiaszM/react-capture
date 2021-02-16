5bd109fdd0fbd535e9523e799686762495329dd8
tobiasz.mateja <tobiasz00@protonmail.com>
2021-02-16 20:24:26 +0100

    router + our work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import AboutUS from './pages/AboutUs'
 import GlobalStyle from './components/GlobalStyle';
-
+import Nav from './components/Nav';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+import {Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <AboutUS />
+      <Nav/>
+      <Switch>
+        <Route exact path="/">
+          <AboutUS />
+        </Route>
+        <Route path="/work">
+          <OurWork/>
+        </Route>
+        <Route path="/contact">
+          <ContactUs/>
+        </Route>
+      </Switch>
     </div>
   );
 }
